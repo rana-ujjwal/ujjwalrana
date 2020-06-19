a30c14813ed58ba1216fa68959652ff6e162b818
rana-ujjwal <67116371+rana-ujjwal@users.noreply.github.com>
2020-06-19 12:36:16 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+//toggle-button
+  const toggleButton = document.getElementsByClassName('toggle-button')[0]
+  const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+  toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+  })
+
+
+/*---go top button start---*/
+
+//Get the button
+  var mybutton = document.getElementById("myBtn");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+/*---go top button end---*/
+
+// theme change 
+  function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+
